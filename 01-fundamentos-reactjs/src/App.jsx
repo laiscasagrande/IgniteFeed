f9486c861ca330c095f5a8a42e89b30d63f56c5d
laiscasagrande <laisluanaka@outlook.com>
2024-06-07 21:53:26 -0300

    :ok_hand: style: CSS das páginas

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,24 @@
 import { Post } from './Post';
+import { Header } from './components/header';
+
+import styles from './App.module.css'
+import './global.css'
 
 export function App() {
   return (
     <>
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates nemo saepe harum quas commodi soluta, minus laborum. Reprehenderit fuga cum dolor id, molestias, provident eum quaerat velit facilis nulla expedita." />
-      <Post
-        author="Gabriel Fernandes"
-        content="Um novo post muitolegal" />
+      <Header />
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main> <Post
+          author="Diego Fernandes"
+          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates nemo saepe harum quas commodi soluta, minus laborum. Reprehenderit fuga cum dolor id, molestias, provident eum quaerat velit facilis nulla expedita." />
+          <Post
+            author="Gabriel Fernandes"
+            content="Um novo post muitolegal" /></main>
+      </div>
     </>//todos os elementos devem estar dentro de um elemento
   )
 }
